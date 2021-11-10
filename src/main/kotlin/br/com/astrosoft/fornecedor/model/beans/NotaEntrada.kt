a9aca8276d0c9295692519f879a01dc468cfab2d
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-11-10 09:19:38 -0300

    Criação da coluna consumo separada da leitura

--- src/main/kotlin/br/com/astrosoft/fornecedor/model/beans/NotaEntrada.kt
+++ src/main/kotlin/br/com/astrosoft/fornecedor/model/beans/NotaEntrada.kt
@@ -4,18 +4,24 @@ import br.com.astrosoft.fornecedor.model.saci
 import java.time.LocalDate
 
 data class NotaEntrada(val invno: Int,
-                  val loja: Int,
-                  val siglaLoja: String,
-                  val dataNF: LocalDate,
-                  val nota: String,
-                  val valor: Double,
-                  val obs: String,
-                  val vendno: Int,
-                  val dataVencimento: LocalDate?) {
+                       val loja: Int,
+                       val siglaLoja: String,
+                       val dataNF: LocalDate,
+                       val nota: String,
+                       val valor: Double,
+                       val obs: String,
+                       val vendno: Int,
+                       val dataVencimento: LocalDate?) {
   fun listFiles() = saci.selectFile(this)
 
-  val consumo
+  private val consumoDireto
     get() = "CONSUMO: *([0-9]+)?".regexGroup(obs)
+  private val consumoLeitura
+    get() = "LIDO *[0-9]+ ?- ?[0-9]+ ?: ?([0-9]*)".regexGroup(obs)
+  val consumo
+    get() = consumoDireto ?: consumoLeitura
+  val lido
+    get() = "LIDO *([0-9]+ ?- ?[0-9]+) ?: ?[0-9]*".regexGroup(obs)
   val demanda
     get() = "DEMANDA: *([0-9/]+)?".regexGroup(obs)
   val periodo
@@ -24,8 +30,6 @@ data class NotaEntrada(val invno: Int,
     get() = "REF.? *([0-9/]+)?".regexGroup(obs)
   val cod
     get() = "COD.? *([0-9\\-]+)?".regexGroup(obs)
-  val lido
-    get() = "LIDO *([0-9]+ ?- ?[0-9]+ ?: ?[0-9]*)".regexGroup(obs)
 
   private fun String.regexGroup(text: String, pos: Int = 1) = this.toRegex().find(text)?.groupValues?.getOrNull(pos)
 }
